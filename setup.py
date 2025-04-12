02fea7fbb68270ea9d73e4907daf10a5f5af981e
CHYang25 <chris920325@gmail.com>
2025-04-13 02:03:22 +0800

    update

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'game2048',
+  packages = find_packages(),
+)
--- student_agent.py
+++ student_agent.py
@@ -21,33 +21,50 @@ def create_env_from_state(state, score):
     sim_env.score = score
     return sim_env
 
-MAX_DEPTH = 10
+MAX_DEPTH = 1
 
-def expectimax(state, score, depth=0):
-    if depth == MAX_DEPTH:
+def expectimax(state, score, legal_actions, depth=0):
+    if depth == MAX_DEPTH or len(legal_actions) == 0:
+        # print(score, approximator.value(state))
         return score + approximator.value(state), -1
     
     new_scores = []
     if depth % 2 == 0: 
-        for action in range(4):
+        for action in legal_actions:
             sim_env = create_env_from_state(state, score)
             next_state, reward, done, _ = sim_env.step(action)
-            sc, at = expectimax(next_state, reward + score, depth=depth+1)
+            legal_next_actions = [a for a in range(4) if sim_env.is_move_legal(a)]
+            sc, at = expectimax(next_state, sim_env.score, legal_next_actions, depth=depth+1)
             new_scores.append(sc)
-        return max(new_scores), np.argmax(new_scores)
+        return max(new_scores), legal_actions[np.argmax(new_scores)]
     else:
-        for action in range(4):
+        for action in legal_actions:
             sim_env = create_env_from_state(state, score)
             next_state, reward, done, _ = sim_env.step(action)
-            sc, at = expectimax(next_state, reward + score, depth=depth+1)
+            legal_next_actions = [a for a in range(4) if sim_env.is_move_legal(a)]
+            sc, at = expectimax(next_state, sim_env.score, legal_next_actions, depth=depth+1)
             new_scores.append(sc)
-        return sum(new_scores) / 4, -1
+        return sum(new_scores) / len(new_scores), -1
 
 
 def get_action(state, score):
-    new_score, action = expectimax(state, score)
+    sim_env = create_env_from_state(state, score)
+    legal_actions = [a for a in range(4) if sim_env.is_move_legal(a)]
+    new_score, action = expectimax(state, score, legal_actions)
     return action
 
 
 if __name__ == '__main__':
-    print(approximator)
+    state = env.reset()
+    done = False
+    cnt = 0
+
+    while not done:
+
+        action = get_action(state, env.score)
+        
+        print("Step:", cnt, "Score:", env.score)
+        print("State:\n", state)
+
+        state, reward, done, _ = env.step(action)
+        cnt += 1
